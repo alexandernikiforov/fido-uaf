1311f9dde82fd7a737c87795ff21184bb9b1f27e
Alexander Nikiforov <admin@ilab.home>
2019-01-24 00:01:16 +0100

    Fixed the build

--- fido-uaf-protocol-v11/src/test/java/ch/alni/fido/uaf/protocol/v1_1/AuthenticationRequestTest.java
+++ fido-uaf-protocol-v11/src/test/java/ch/alni/fido/uaf/protocol/v1_1/AuthenticationRequestTest.java
@@ -1,5 +1,6 @@
 package ch.alni.fido.uaf.protocol.v1_1;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.commons.io.IOUtils;
@@ -9,8 +10,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
-
-import ch.alni.fido.uaf.Json;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,11 +22,11 @@ public class AuthenticationRequestTest {
                 StandardCharsets.UTF_8
         );
 
-//        final List<AuthenticationRequest> list = new ObjectMapper().readValue(
-//                authRequestAsJson,
-//                new TypeReference<List<AuthenticationRequest>>() {}
-//        );
-        final List<AuthenticationRequest> list = Json.ofJsonString(authRequestAsJson);
+        final List<AuthenticationRequest> list = new ObjectMapper().readValue(
+                authRequestAsJson,
+                new TypeReference<List<AuthenticationRequest>>() {
+                }
+        );
 
         assertThat(list).hasSize(1);
         assertThat(list.get(0)).isInstanceOf(AuthenticationRequest.class);
